7bb0b25bd17d812bc8facf91c3ca198cf41ac4e5
vendethiel <vendethiel@hotmail.fr>
2016-02-26 16:59:05 +0100

    Add sql stuff

--- ful.go
+++ ful.go
@@ -5,17 +5,43 @@ import (
 	"net/http"
   "log"
   "fmt"
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql"
 )
+
+type User struct {
+  id int
+  firstname, lastname, email, password, role string
+}
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.WriteHeader(200)
 }
 
 func UserShow(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  fmt.Fprintf(w, "Yo %s\n", p.ByName("id"))
+  err := db.QueryRow("SELECT firstname, lastname, email, password, role FROM user WHERE id = ?", p.ByName("id"))
+}
+
+func connectToDb() (*sql.DB, error) {
+  db, err := sql.Open("mysql", "root:root@/ful")
+  if err != nil {
+    return nil, err
+  }
+  defer db.Close()
+
+  err = db.Ping()
+  if err != nil {
+    return nil, err
+  }
+  return db, nil
 }
 
 func main() {
+  _, err := connectToDb()
+  if err != nil {
+    panic(err.Error())
+  }
+
   router := httprouter.New()
   router.GET("/", Index)
   router.GET("/users/:id", UserShow)
