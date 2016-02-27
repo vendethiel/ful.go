e6113a0e30b504abb691b0b6e87e2db97f379bd4
vendethiel <vendethiel@hotmail.fr>
2016-02-27 19:28:40 +0100

    style

--- ful.go
+++ ful.go
@@ -1,32 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
-  "log"
-  //"fmt"
-  "database/sql"
-  "encoding/json"
+	//"fmt"
+	"database/sql"
+	"encoding/json"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
-  _ "github.com/go-sql-driver/mysql"
 )
 
 type JsonError struct {
-  Status int `json:"status"`
-  Message string `json:"message"`
+	Status  int    `json:"status"`
+	Message string `json:"message"`
 }
 
 func markAsJson(w http.ResponseWriter) {
-  w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 }
 
 func sendJson(w http.ResponseWriter, o interface{}) error {
-  if json, err := json.Marshal(o); err == nil {
-    w.Write(json)
-    return nil
-  } else {
-    return err
-  }
+	if json, err := json.Marshal(o); err == nil {
+		w.Write(json)
+		return nil
+	} else {
+		return err
+	}
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -34,50 +34,50 @@ func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
 func UserShow(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-  return func(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
-    markAsJson(w)
-    if user, err := GetUserBy(db, "id", p.ByName("id")); err != nil {
-      w.WriteHeader(404)
-    } else if user.Role == "admin" {
-      w.WriteHeader(401)
-      sendJson(w, JsonError{404, "not found"})
-    } else {
-      sendJson(w, user)
-    }
-  }
+	return func(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
+		markAsJson(w)
+		if user, err := GetUserBy(db, "id", p.ByName("id")); err != nil {
+			w.WriteHeader(404)
+		} else if user.Role == "admin" {
+			w.WriteHeader(401)
+			sendJson(w, JsonError{404, "not found"})
+		} else {
+			sendJson(w, user)
+		}
+	}
 }
 
 func UserSearch(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-  return func(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
-    
-  }
+	return func(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
+
+	}
 }
 
 func connectToDb() (*sql.DB, error) {
-  db, err := sql.Open("mysql", "root:root@/ful")
-  if err != nil {
-    return nil, err
-  }
+	db, err := sql.Open("mysql", "root:root@/ful")
+	if err != nil {
+		return nil, err
+	}
 
-  err = db.Ping()
-  if err != nil {
-    return nil, err
-  }
-  return db, nil
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
 
 func main() {
-  db, err := connectToDb()
-  if err != nil {
-    panic(err.Error())
-  }
-  defer db.Close()
+	db, err := connectToDb()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
-  router := httprouter.New()
-  router.GET("/", Index)
-  router.GET("/users/:id", UserShow(db))
-  router.GET("/search/users", UserSearch(db))
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/users/:id", UserShow(db))
+	router.GET("/search/users", UserSearch(db))
 
-  log.Print("Starting....\n")
+	log.Print("Starting....\n")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
