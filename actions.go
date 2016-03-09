820f2f396d2b69c4b1903dd2bb4d07f88d96d758
vendethiel <vendethiel@hotmail.fr>
2016-03-09 12:56:40 +0100

    Add authentication

--- actions.go
+++ actions.go
@@ -3,11 +3,49 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"encoding/base64"
 	"io/ioutil"
 	"net/http"
+  "strings"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+type authHandler func(*User, http.ResponseWriter, *http.Request, httprouter.Params)
+
+func Authenticate(db *sql.DB, action authHandler) func(http.ResponseWriter, *http.Request, httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+    // validate we have the header...
+    authHeader := r.Header["Authorization"]
+    if len(authHeader) == 0 {
+      w.WriteHeader(401)
+      return
+    }
+    // validate the header's shape...
+    auth := strings.SplitN(authHeader[0], " ", 2)
+    if len(auth) != 2 || auth[0] != "Basic" {
+      w.WriteHeader(400)
+      return
+    }
+
+    // parse the header... validate the size
+    payload, _ := base64.StdEncoding.DecodeString(auth[1])
+    pair := strings.SplitN(string(payload), ":", 2)
+    if len(pair) != 2  {
+      w.WriteHeader(400)
+      return
+    }
+    // validate the user
+    user, err := GetUserBy(db, "email", pair[0])
+    if err != nil || user.Password != pair[1] {
+      w.WriteHeader(401)
+      return
+    }
+
+    // call the action with the authentified user
+    action(&user, w, r, p)
+  }
+}
 
 func parseUser(r *http.Request) (User, error) {
 	body, err := ioutil.ReadAll(r.Body)
@@ -24,8 +62,8 @@ func parseUser(r *http.Request) (User, error) {
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 }
 
-func UserShow(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-	return func(w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
+func UserShow(db *sql.DB) authHandler {
+	return func(user *User, w http.ResponseWriter, _ *http.Request, p httprouter.Params) {
 		MarkAsJSON(w)
 		if user, err := GetUserBy(db, "id", p.ByName("id")); err != nil {
 			w.WriteHeader(404)
@@ -38,8 +76,8 @@ func UserShow(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.Pa
 	}
 }
 
-func UserSearch(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func UserSearch(db *sql.DB) authHandler {
+	return func(u *User, w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		MarkAsJSON(w)
 		email := r.URL.Query().Get("q")
 		if len(email) == 0 {
@@ -52,15 +90,18 @@ func UserSearch(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.
 	}
 }
 
-func UserCreate(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func UserCreate(db *sql.DB) authHandler {
+	return func(u *User, w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		MarkAsJSON(w)
 		user, err := parseUser(r)
 		if err != nil {
 			SendError(w, err)
 			return
 		}
-		if user.Role != "normal" && user.Role != "admin" {
+    // only two roles allowed: normal and admin
+    // also prevent non-admins from creating admin users
+		if (user.Role != "normal" && user.Role != "admin") ||
+       u.Role != "admin" {
 			user.Role = "normal"
 		}
 		newUser, err := InsertUser(db, user)
@@ -73,8 +114,8 @@ func UserCreate(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.
 	}
 }
 
-func UserUpdate(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func UserUpdate(db *sql.DB) authHandler {
+	return func(u *User, w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		id := p.ByName("id")
 		user, err := parseUser(r)
 		if err != nil {
@@ -94,13 +135,16 @@ func UserUpdate(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.
       UpdateUserColumn(db, id, "password", user.Password)
     }
     if user.Role == "normal" || user.Role == "admin" {
-      UpdateUserColumn(db, id, "role", user.Role)
+      // non-admin can't update other users' roles
+      if u.Role == "admin" {
+        UpdateUserColumn(db, id, "role", user.Role)
+      }
     }
 	}
 }
 
-func UserRemove(db *sql.DB) func(http.ResponseWriter, *http.Request, httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+func UserRemove(db *sql.DB) authHandler {
+	return func(u *User, w http.ResponseWriter, r *http.Request, p httprouter.Params) {
     if err := DeleteUser(db, p.ByName("id")); err != nil {
       SendError(w, err)
       return
--- ful.go
+++ ful.go
@@ -31,11 +31,11 @@ func main() {
 
 	router := httprouter.New()
 	router.GET("/", Index)
-	router.GET("/users/:id", UserShow(db))
-	router.GET("/search/users", UserSearch(db))
-	router.POST("/users", UserCreate(db))
-	router.PUT("/users/:id", UserUpdate(db))
-	router.DELETE("/users/:id", UserRemove(db))
+	router.GET("/users/:id", Authenticate(db, UserShow(db)))
+	router.GET("/search/users", Authenticate(db, UserSearch(db)))
+	router.POST("/users", Authenticate(db, UserCreate(db)))
+	router.PUT("/users/:id", Authenticate(db, UserUpdate(db)))
+	router.DELETE("/users/:id", Authenticate(db, UserRemove(db)))
 
 	log.Print("Starting....\n")
 	log.Fatal(http.ListenAndServe(":8080", router))
